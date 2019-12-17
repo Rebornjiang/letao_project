b8ab051df4d827571ff8d2d3d3dc41ace80ace20
Rebornjiang <626104624@qq.com>
2019-12-17 17:24:08 +0800

    上拉刷新下拉加载完成

--- public/front/js/search_list.js
+++ public/front/js/search_list.js
@@ -1,5 +1,6 @@
 $(function () {
-
+  var currentPage = 1;
+  var pageSize = 2;
   // 添加搜索排序功能点击实现高亮
   // $('.sort a').click(function () {
   //   $(this).addClass('current').parent().siblings().find('a').removeClass('current')
@@ -9,11 +10,47 @@ $(function () {
   let searchKey = getSearch('key')
   $('.lt_search .header input').val(searchKey)
 
+  // 下拉刷新功能
+  mui.init({
+    pullRefresh: {
+      container: ".mui-scroll-wrapper",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+      down: {
+        //  刷新的过程中需要做那些事情//必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+        callback: function () {
+          currentPage = 1;
+          render(function(info) {
+            $(".product ul").html(template('tpl', info))
+            mui('.mui-scroll-wrapper').pullRefresh().endPulldownToRefresh()
+            mui('.mui-scroll-wrapper').pullRefresh().endPullupToRefresh(false)
+          })
+          // 刷新不会自动结束，需要调用结束的函数该方法的作用是关闭“正在刷新”的样式提示，内容区域回滚顶部位置，
+          // mui('.mui-scroll-wrapper').pullRefresh().endPullDownRefresh();
+        }
+      },
+      up: {
+        // 上拉加载刷新时需要执行的函数
+        callback: function () {
+          currentPage++
+          render(function(info) {
+            
+            $(".product ul").append(template('tpl', info))
+            if (info.data.length === 0) {
+
+              mui('.mui-scroll-wrapper').pullRefresh().endPullupToRefresh(true)
+            }else {
+              mui('.mui-scroll-wrapper').pullRefresh().endPullupToRefresh(false)
+              
+            }
+          })
+          
+        }
+      }
+    }
+  });
 
 
-
-  $('.sort a[data-type]').click(function () {
-
+  // 2.判断是否有current类，如果有更改当前元素的箭头为相反方向，如果没有的话增加上current的类。
+  $('.sort a[data-type]').on('tap',function() {
     if ($(this).hasClass('current')) {
       //如果有current类就改变箭头 ，也就是i标签的 类
       $(this).find('i').toggleClass("fa-angle-up").toggleClass("fa-angle-down")
@@ -21,22 +58,26 @@ $(function () {
       $(this).addClass('current').parent().siblings().find('a').removeClass('current')
     }
 
-
-
-    render()
+    render(function(info) {
+      mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading()
+    })
   })
 
-  // 功能2 动态渲染页面，发送请求获取数据
-  render()
-  function render() {
 
-    $('.product ul').html('<div class="loading"></div>')
+  // 功能2 动态渲染页面，发送请求获取数据
+  render(function(info) {
+    mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading()
+  })
+
+  function render(callback) {
+
+    // $('.product ul').html('<div class="loading"></div>')
 
     // 功能 4 实现排序与降序 
     let params = {
       proName: $('.lt_search .header input').val(),
-      page: 1,
-      pageSize: 100
+      page: currentPage,
+      pageSize: pageSize
     }
     // price 使用价格排序（1升序，2降序）
     // num   产品库存排序（1升序，2降序）
@@ -53,15 +94,16 @@ $(function () {
       params[currentKey] = currentVal
 
     }
+
     setTimeout(function () {
       $.ajax({
         url: '/product/queryProduct',
         type: 'get',
         data: params,
         success(info) {
-          console.log(info)
-
-          $(".product ul").html(template('tpl', info))
+          // console.log(info)
+          callback && callback(info)
+         
         }
       })
     }, 500)
@@ -77,7 +119,10 @@ $(function () {
       return
     }
 
-    render()
+    render(function(info) {
+      mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading()
+    })
+
     // 4. 需要将搜索的内容添加到localStorage中search_list中
     let jsonStr = localStorage.getItem('search_history_list')
     let arr = JSON.parse(jsonStr)
@@ -97,6 +142,14 @@ $(function () {
     localStorage.setItem('search_history_list', newJsonStr)
   })
 
+
+  // 因为<!-- 商品下拉会禁止a连接的点击事件 --> 解决点击图片可以跳转下一页的方法就是，给a绑定
+  // tap事件
+  
+  $(".product ul").on('tap','a',function() {
+    let id = $(this).attr('data-id')
+    location.href = "product_detail.html?productId=" + id
+  })
 
 
 
